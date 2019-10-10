71cab644e1ce1a30a2d231c94b03e9ebbb1d3537
codebender828 <jbakebwa@163.com>
2019-10-11 03:27:53 +0800

    add: linux commands comments

--- utils/logger.h
+++ utils/logger.h
@@ -1,6 +1,8 @@
 #include <sys/time.h>
+#include <stdio.h>
 
 void logProcess(char * name, int processStartTime, struct timeval * processEndTime) {
   gettimeofday(processEndTime, NULL);
+  // In Linux, Te `%d` should be changed to `%ld` for this logger to work.
   printf("%s process life span: %d ms\n", name, (processEndTime->tv_usec - processStartTime));
 }
